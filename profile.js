e5af5106bd43b0b5a5a7583816de50a6d7b9753b
Abinithi-Certainti <abinithi.govindaraj@certainti>
2026-03-20 15:11:50 +0530

    t

---
+++ profile.js
@@ -0,0 +1,17 @@
+const name = "Abi";
+let age = 22;
+const isDeveloper = true;
+
+let project = null;
+let experience;
+
+const user = { name, age, skills: ["JavaScript", "Node.js"] };
+
+console.log(`Hi, I'm ${name} and I am ${age} years old.`);
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of project:", typeof project);
+
+let value = "hello";
+value = 100;
+console.log("value:", value);
